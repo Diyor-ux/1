9f8b9928d1efc800c1313832866f060f267096e3
Diyor-ux <dr20080430@gmail.com>
2025-06-22 12:45:38 +0500

    Delete next.config.js

--- next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  // boshqa sozlamalar bo‘lsa, ularni qoldiring
-  experimental: {
-    allowedDevOrigins: ['http://66.42.97.211:3000']
-  }
-}
-
-module.exports = nextConfig
